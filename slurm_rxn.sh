7fdac0e8b5203b1bb251b1eb071f69ade6afd5eb
aberdeendinius <dinius.ab@husky.neu.edu>
2019-01-22 10:09:14 -0500

    Richard's attempt to get SLURM working

---
+++ slurm_rxn.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+## Run the $gaussianfile in g16
+
+#time you think you need; default is one day
+# d-hh:mm:ss
+##SBATCH --time=0
+
+#number of tasks you are requesting
+#SBATCH -N 1
+#SBATCH -n 20
+##SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
+##SBATCH --mem=110GB
+
+#partition to use.
+#SBATCH --partition=west
+
+#an array for the job.
+#SBATCH --array=1
+
+#####################################################
+g16 $gaussianfile 
