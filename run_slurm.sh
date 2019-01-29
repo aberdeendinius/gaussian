793c91dc0f9622a09e23f03dbf427e4ad3ff9ccc
aberdeendinius <dinius.ab@husky.neu.edu>
2019-01-29 09:08:38 -0500

    Updated slurm scripts

--- run_slurm.sh
+++ run_slurm.sh
@@ -2,11 +2,14 @@ for f in *; do
 	fname="${f%.*}"
 	ext="${f##*.}"
 	if [ $ext = "com" ]
+	now = (date +"%m-%d-%T")
+	printName=$fname+$now
+	echo "$printName"
 	then
 		echo "$f"
-		#mkdir  "$fname"
-		cd "$fname"
-		sbatch sample_slurm.sh ../"$f"
+		mkdir  "$printName"
+		#cd "$printName"
+		#sbatch --job-name=$fname --output=$printName.log --error=$printName.slurm.log sample_slurm.sh ../"$f"
 		cd ..
 	fi
 done
--- sample_slurm.sh
+++ sample_slurm.sh
@@ -1,29 +1,20 @@
 #!/bin/sh
-#set a job name
-#SBATCH --job-name=$1
-#a file for job output, you can check job progress
-#SBATCH --output=$1.log
-
-# a file for errors from the job
-#SBATCH --error=$1.slurm.log
-
-#time you think you need; default is one day
-# d-hh:mm:ss
-##SBATCH --time=0
 
 #number of tasks you are requesting
 #SBATCH -N 1
 #SBATCH -n 20
 ##SBATCH --ntasks-per-node=1
-#SBATCH --exclusive
-##SBATCH --mem=110GB
+#SBATCH --mem=120000
 
 #partition to use.
-#SBATCH --partition=west
+#SBATCH --partition=general
 
 #an array for the job.
 #SBATCH --array=1
 
 
 #####################################################
-g16 $1 
+module unload openmpi/3.1.2
+module unload openmpi/3.1.1
+module load openmpi/2.0.4
+/shared/apps/orca/orca_4_0_1_linux_x86-64_openmpi202/orca $1 
