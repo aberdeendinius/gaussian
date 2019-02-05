4cdd31c0327542fd5715d016af365798df0955b0
aberdeendinius <dinius.ab@husky.neu.edu>
2019-02-05 09:25:36 -0500

    Updated submit script runrxn.sh
    You now call it like this:
    $ ./runrxn.sh path/to/gaussian/input.com
    
    like
    $ ./runrxn.sh rxn905/rxn905.com

--- runrxn.sh
+++ runrxn.sh
@@ -3,13 +3,19 @@
 ## Sets up reaction-specific names etc.
 ## and submits the slurm_rxn to SLURM
 
-export jobname="rxn"$1
-export gaussianfile=$jobname.com
+## Run it with the path to the gaussian file, like this:
+## $ ./runrxn.sh jobs/rxn907/rxn907-final.com
+
+export filename=$(basename "$1")  # eg. rxn907-final.com
+export jobname="${filename%.*}"   # eg. rxn907-final
+export directory=$(dirname "$1")  # eg. jobs/rxn907
+
+export gaussianfile=$filename
 #####################################################
-sbatch --chdir=$jobname \
+sbatch --chdir=$directory \
  --job-name=$jobname \
  --error=$jobname.error.log \
  --output=$jobname.log \
- --export=ALL,gaussianfile=$jobname.com \
+ --export=ALL,gaussianfile=$filename\
  slurm_rxn.sh ;
-echo "Submitted "$jobname
+echo "Submitted $filename in $directory with job name $jobname"
